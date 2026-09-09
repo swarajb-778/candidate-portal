f8eff3e4b4f4a476c1ad790728ac8f436e5984af
swarajb-778 <swarajbangar77@gmail.com>
2026-09-08 22:54:27 -0700

    feat: re-export models from one module

---
+++ server/src/models/index.js
@@ -0,0 +1,7 @@
+export { User } from './User.js';
+export { Application } from './Application.js';
+export { Interview } from './Interview.js';
+export { Document } from './Document.js';
+export { Notification } from './Notification.js';
+export { Thread, Message } from './Thread.js';
+export { Offer } from './Offer.js';
